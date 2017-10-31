54a7cf273b626beb49c6db4e8d7537002b85bff3
chadroemer <croemer13@winona.edu>
2017-10-31 11:35:14 -0500

    Update proteinDB.sql

--- WebContent/SQL_Scripts/proteinDB.sql
+++ WebContent/SQL_Scripts/proteinDB.sql
@@ -26,6 +26,80 @@ CREATE TABLE `protein` (
   PRIMARY KEY (`protein_ID`)
   );
   
+  CREATE TABLE IF NOT EXISTS `Disease` (
+  `dis_id` varchar (25),
+  `name` varchar(25),
+   `type` varchar(25),
+    `prognosis` varchar(25),
+     `treatment` varchar(25),
+      `assoc_gene` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `Pathogenicity` (
+  `dis_id` varchar (25),
+  `prot_id` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `Cofactor` (
+  `prot_id` varchar (25),
+  `ligand_id` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `Ligand` (
+  `ligand_id` varchar (25),
+  `mol_weight` varchar(25),
+   `class` varchar(25),
+    `polarity` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `Substrate` (
+  `prot_id` varchar (25),
+  `kinetics_id` varchar(25),
+   `ligand_id` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `Protein` (
+  `prot_id` varchar (25),
+  `name` varchar(25),
+   `pl` varchar(25),
+    `family` varchar(25),
+     `localization` varchar(25),
+     `gene_loc` varchar(25),
+      `size` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `Amino Acid` (
+  `letter_code` varchar (25),
+  `tri_letter_code` varchar(25),
+   `polarity` varchar(25),
+    `charge` varchar(25),
+     `n_pka` varchar(25),
+      `r_pka` varchar(25),
+      `c_pka` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `kinetics_prof` (
+  `kinetics_id` varchar (25),
+  `Vmax` varchar(25),
+   `km` varchar(25),
+    `kcat` varchar(25),
+     `treatment` varchar(25),
+      `assoc_gene` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+
+CREATE TABLE IF NOT EXISTS `PDBfile` (
+  `id` varchar (25),
+  `filepath` varchar(25),
+   `size` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+
+CREATE TABLE IF NOT EXISTS `3D_rep` (
+  `pdb_id` varchar (25),
+      `prot_id` varchar(25)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  
   
   
   
